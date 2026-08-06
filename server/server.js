b7a2be26203b5ad517bad6a831d908a24f8b4696
animeshpramanik719-netizen <animeshpramanik719@gmail.com>
2026-08-06 15:22:37 +0530

    new 2

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+app.get("/",(req,res)=> res.send("Server is running")); 
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
